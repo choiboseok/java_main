0d81846bbdd150896506dfc12816e5059433b029
choiboseok <117703298+choiboseok@users.noreply.github.com>
2024-07-25 21:23:56 +0900

    Add files via upload
    20230725 java use bean

---
+++ MyWorkspace/jwbook/src/main/java/ch05/HelloWorld.java
@@ -0,0 +1,49 @@
+package ch05;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");// 응답 콘텐츠 타입 설정과 한글 출력 
+		PrintWriter out = response.getWriter(); //java.io.PrintWriter 클래스 타입의 객체 리턴
+			out.append("<!doctype html><html><head><title>Hello World Servlet</title></head><body>")
+				.append("<h2>Hello World !!</h2><hr>")
+				.append("현재 날짜와 시간은: "+java.time.LocalDate.now())
+				.append("</body></html>");
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
